e4377ae4e8f25ff27f7d092f8d91c93a7fa3f949
Sami Askar <saskar301@gmail.com>
2022-03-03 15:05:17 +0100

    Added aave interfaces

--- scripts/aave_borrow.py
+++ scripts/aave_borrow.py
@@ -1,5 +1,5 @@
 from scripts.helpful_scripts import get_account
-from brownie import network, config
+from brownie import network, config, interface
 from scripts.get_weth import get_weth
 
 
@@ -8,3 +8,12 @@ def main():
     erc20_address = config["networks"][network.show_active()]["weth_token"]
     if network.show_active() in ["mainnet-fork"]:
         get_weth()
+    # need abi and address
+    lending_pool = get_lending_pool()
+
+
+def get_lending_pool():
+    lending_pool_address_provider = interface.ILendingPoolAddressesProvider(
+        config["networks"][network.show_active()]["lending_pool_addresses_provider"]
+    )
+    lending_pool_address = lending_pool_address_provider.getLendingPool()
