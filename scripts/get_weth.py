f48f3bf6731630c98a656aa7a41224daea9122ae
Sami Askar <saskar301@gmail.com>
2022-03-03 13:32:05 +0100

    added code to borrow.py

--- scripts/get_weth.py
+++ scripts/get_weth.py
@@ -12,4 +12,5 @@ def get_weth():
         config["networks"][network.show_active()]["weth_token"]
     )
     tx = weth.deposit({"from": account, "value": 0.01 * 10**18})
+    tx.wait(1)
     print("Received 0.1 WETH")
